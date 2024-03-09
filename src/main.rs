f387dde661f38edba276918796b2e0c500fba45e
daniel david kovacs <kicsikrumpli@gmail.com>
2024-03-09 08:32:23 +0100

    hello, world

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <p> {"Hello, world!"} </p>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
